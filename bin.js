71a3023811cdacaee908eca5ae106cf080e0fed6
Julian Gruber <julian@juliangruber.com>
2017-03-31 11:23:28 +0200

    initial commit

---
+++ bin.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+'use strict'
+
+const differ = require('ansi-diff-stream')
+const render = require('render-ci-matrix')()
+const resolve = require('path').resolve
+const fs = require('fs')
+const Watch = require('.')
+
+const diff = differ()
+diff.pipe(process.stdout)
+
+const dir = resolve(process.argv[2] || '.')
+
+try {
+  fs.statSync(dir)
+} catch (err) {
+  console.error('Usage: ci-watch [DIRECTORY]')
+  process.exit(1)
+}
+
+try {
+  fs.statSync(`${dir}/.travis.yml`)
+} catch (_) {
+  try {
+    fs.statSync(`${dir}/appveyor.yml`)
+  } catch (_) {
+    console.error('CI not set up. Skipping...')
+    process.exit(0)
+  }
+}
+
+const update = () => {
+	diff.reset()
+	diff.write(render(watch.state()))
+}
+
+const watch = new Watch(dir)
+watch.start()
+watch.on('finish', () => {
+	update()
+  process.exit(!watch.state.success)
+})
+
+setInterval(update, 100)
---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const fs = require('fs')
+const EventEmitter = require('events')
+const inherits = require('util').inherits
+const deep = require('deep-access')
+
+const CI = {
+  travis: require('travis-watch'),
+  appVeyor: require('appveyor-watch')
+}
+
+module.exports = Watch
+inherits(Watch, EventEmitter)
+
+function Watch (dir) {
+  if (!(this instanceof Watch)) return new Watch(dir)
+  EventEmitter.call(this)
+  this._dir = dir
+  this._ci = {}
+  this._started = new Date()
+}
+
+Watch.prototype.state = function () {
+  const one = key => {
+    let val
+    Object.keys(this._ci).forEach(name => {
+      val = val || deep(this._ci[name].state, key)
+    })
+    return val
+  }
+
+  const all = key => Object.keys(this._ci).map(name => deep(this._ci[name].state, key))
+
+  return {
+    started: this.started,
+    commit: {
+      sha: one('commit.sha'),
+      found: one('commit.found'),
+      branch: one('commit.branch')
+    },
+    link: all('link').join('\n'),
+    repo: one('repo'),
+    build: one('build') && {
+      number: all('build?.number').filter(Boolean).join('/')
+    },
+    results: all('results').reduce((acc, results) => {
+      Object.keys(results).forEach(os => {
+        acc[os] = results[os]
+      })
+      return acc
+    }, {}),
+    success: all('success').reduce((acc, success) =>
+      typeof acc === 'boolean' && !acc
+        ? acc
+        : success)
+  }
+}
+
+Watch.prototype._checkEnvironment = function (cb) {
+  fs.stat(`${this._dir}/.travis.yml`, noTravis => {
+    fs.stat(`${this._dir}/appveyor.yml`, noAppVeyor => {
+      cb(null, {
+        travis: !noTravis,
+        appVeyor: !noAppVeyor
+      })
+    })
+  })
+}
+
+Watch.prototype.start = function () {
+  this._checkEnvironment((err, ci) => {
+    if (err) return this.emit('error', err)
+    let todo = Object.keys(ci).length
+    const onfinish = () => {
+      if (!--todo) this.emit('finish')
+    }
+    Object.keys(ci).forEach(name => {
+      if (!ci[name]) return
+      const watch = this._ci[name] = CI[name](this._dir)
+      watch.start()
+      watch.on('finish', onfinish)
+    })
+  })
+}
